0cd1a06d076ac158e7a82c36ea4101b0c8ded2f7
soheil_dev <soheil.torkan2222@gmail.com>
2025-10-01 17:42:53 +0330

    add some option to project

--- java script/todo.js
+++ java script/todo.js
@@ -1,3 +1,4 @@
+//Add some vars
 const themeSwitcher = document.querySelector(".switch-theme");
 const myBody = document.querySelector("body");
 const iconTheme = themeSwitcher.children[0];
@@ -6,9 +7,12 @@ const inputText = document.querySelector(".text-list");
 const addListBtn = document.querySelector(".addlist");
 const showError = document.querySelector(".show-error ");
 const theList = document.querySelector(".list");
-const deleteEL = document.querySelectorAll(".delete");
+const countItems = document.querySelector(".count");
+let count = 0;
 
+//Create main function
 function TodoList() {
+    //Add change theme page
     themeSwitcher.addEventListener('click', () => {
         myBody.classList.toggle("light");
         iconTheme.classList.toggle("moon");
@@ -27,6 +31,7 @@ function TodoList() {
     addListBtn.addEventListener('mouseup', () => {
         addListBtn.style.padding = "0.3rem 0.9rem 0.3rem 0.9rem";
     });
+    //Add drag property
     theList.addEventListener('dragover', (e) => {
         if (e.target.classList.contains("my-items") && !e.target.classList.contains("dragging")) {
             const draggingItem = document.querySelector(".dragging");
@@ -35,16 +40,18 @@ function TodoList() {
             const newPos = items.indexOf(e.target);
             if (currentPos > newPos) {
                 theList.insertBefore(draggingItem, e.target);
-                const todos = JSON.parse(localStorage.getItem("todos"));
-                const removed = todos.splice(currentPos, 1);
-                todos.splice(newPos, 0, removed[0]);
-                localStorage.setItem("todos", JSON.stringify(todos));
             } else {
                 theList.insertBefore(draggingItem, e.target.nextSibling);
             }
+            const todos = JSON.parse(localStorage.getItem("todos"));
+            const removed = todos.splice(currentPos, 1);
+            todos.splice(newPos, 0, removed[0]);
+            localStorage.setItem("todos", JSON.stringify(todos));
         }
     });
     maketodos(JSON.parse(localStorage.getItem("todos")));
+    itemsOfApp();
+    //Add event for button add
     addListBtn.addEventListener('click', () => {
         const myItem = inputText.value.trim();
         if (myItem) {
@@ -57,6 +64,7 @@ function TodoList() {
             };
             todos.push(currentTodo);
             localStorage.setItem("todos", JSON.stringify(todos));
+            maketodos([currentTodo]);
         } else {
             const textError = document.createElement("p");
             const theInterval = setInterval(() => {
@@ -70,8 +78,10 @@ function TodoList() {
             }, 5000);
         }
     });
+    sendByEnter();
 }
 
+//Create items
 function maketodos(todoArray) {
     todoArray.forEach(todoObject => {
         const newLi = document.createElement("li");
@@ -82,6 +92,12 @@ function maketodos(todoArray) {
         const inputCheck = document.createElement("input");
         inputCheck.className = "choose";
         inputCheck.type = "checkbox";
+        if (todoObject.isComplated == true) {
+            inputCheck.checked = "checked";
+            newLi.classList.add("complated");
+        } else {
+            newLi.classList.remove("complated");
+        }
         const addedText = document.createElement("p");
         addedText.className = "added-text";
         addedText.textContent = todoObject.item;
@@ -100,7 +116,63 @@ function maketodos(todoArray) {
         newLi.addEventListener('dragend', () => {
             newLi.classList.remove("dragging");
         });
+        //Delete items
+        deleteBtn.addEventListener('click', () => {
+            const removedItem = deleteBtn.parentElement;
+            const items = [...document.querySelectorAll(".my-items")];
+            const indexRemove = items.indexOf(removedItem);
+            removedItem.classList.add("fall");
+            removeItem(indexRemove);
+            count--;
+        });
+        //Check items
+        inputCheck.addEventListener('click', () => {
+            const checkedItem = inputCheck.parentElement.parentElement;
+            const checked = inputCheck.checked;
+            const items = [...theList.querySelectorAll(".my-items")];
+            const indexCheck = items.indexOf(checkedItem);
+            containsItem(indexCheck, checked);
+            if (checked == true) {
+                newLi.classList.add("complated");
+                count--;
+                countItems.textContent = count;
+            } else {
+                newLi.classList.remove("complated");
+                count++;
+                countItems.textContent = count;
+            }
+        });
     });
 }
 
+//Add property to create items by enter button
+function sendByEnter() {
+    inputText.addEventListener('keydown', (e) => {
+        if (e.key === "Enter")
+            addListBtn.click();
+    })
+}
+
+function removeItem(index) {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function containsItem(index, isComplated) {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    todos[index].isComplated = isComplated;
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function itemsOfApp() {
+    const items = [...document.querySelectorAll(".my-items")];
+    items.forEach(item => {
+        if (!item.classList.contains("complated"))
+            count++;
+        else
+            count--;
+    })
+    countItems.textContent = count;
+}
 document.addEventListener('DOMContentLoaded', TodoList);
