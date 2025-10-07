7debec6d80f670588aa786072be7ce7df91eee33
soheil_dev <soheil.torkan2222@gmail.com>
2025-10-07 15:30:56 +0330

    add some options

--- java script/NoteApp.js
+++ java script/NoteApp.js
@@ -5,7 +5,8 @@ const iconTheme = document.querySelector(".theme");
 const main = document.getElementById("main");
 const header = document.querySelector(".header");
 const addNote = document.querySelector(".add-note");
-let showError;
+const myNotes = document.querySelector(".notes");
+let count = 0;
 
 function NoteApp() {
     //add theme switcher
@@ -16,11 +17,12 @@ function NoteApp() {
         else
             iconTheme.src = "/picture/icon-sun.svg";
     });
+    makeItemsOfNotes(JSON.parse(localStorage.getItem("notes")));
     //add inputs
     addNote.addEventListener('click', () => {
         const infoNote = document.createElement("div");
         infoNote.className = "info-note d-flex flex-column justify-content-center w-100 gap-3 pt-4";
-        infoNote.innerHTML = `   <label for="title" class="form-label title">عنوان</label>
+        infoNote.innerHTML = `<label for="title" class="form-label title">عنوان</label>
             <input type="text" id="title" class="form-control form-control-lg title-input" placeholder="عنوان...">
             <label for="text" class="form-label text">یاد داشت</label>
             <input type="text" class="form-control form-control-lg text-input" id="text" placeholder="یاد داشت...">
@@ -31,6 +33,7 @@ function NoteApp() {
         const titleInput = document.querySelector(".title-input");
         const textInput = document.querySelector(".text-input");
         const saveNote = document.querySelector(".save-note");
+        let status = true;
         saveNote.addEventListener('click', () => {
             const titleValue = titleInput.value.trim();
             const textValue = textInput.value.trim();
@@ -39,12 +42,13 @@ function NoteApp() {
                 const textError = document.createElement("p");
                 textError.textContent = "لطفا تمام فیلد های بالا را پر کنید.";
                 textError.classList.add("fs-5", "text-danger");
-                showError = setInterval(() => {
+                if (status == true) {
+                    status = false;
                     holderSaveNote.append(textError);
-                }, 100);
+                }
                 setTimeout(() => {
-                    clearInterval(showError);
                     textError.remove();
+                    status = true;
                 }, 5000);
             } else {
                 titleInput.value = "";
@@ -55,10 +59,89 @@ function NoteApp() {
                     text: textValue
                 };
                 notes.push(infoOfNotes);
+                makeItemsOfNotes([infoOfNotes]);
                 localStorage.setItem("notes", JSON.stringify(notes));
             }
         });
+        //create item by enter
+        document.addEventListener('keydown', (e) => {
+            if (e.key === "Enter")
+                saveNote.click();
+        });
     });
+}
+//create items
+function makeItemsOfNotes(value) {
+    value.forEach(val => {
+        const li = document.createElement("li");
+        li.className = "items d-flex flex-column";
+        li.innerHTML = `<div class="info-notes d-flex justify-content-between">
+                    <div class="change d-flex gap-2">
+                        <button class="change-btns d-flex align-items-center gap-2 bin"><i
+                                class="fi fi-rr-trash"></i>حذف</button>
+                        <button class="change-btns d-flex align-items-center gap-2 edit"><i
+                                class="fi fi-rr-file-edit"></i>ویرایش</button>
+                    </div>
+                    <div class="name-note d-flex gap-2">
+                        <i class="fi fi-rr-note"></i>
+                        <p class="count-note text-primary">نوت<span class="count">${count+=1}:</span></p>
+                        <p class="title-note">${val.title}</p>
+                    </div>
+                </div>
+                <div class="div-text-notes d-flex align-items-center p-3 mt-3">
+                    <p class="text-notes">${val.text}</p>
+                </div>`;
+        myNotes.append(li);
+        //remove note
+        const bin = li.querySelector(".bin");
+        bin.addEventListener('click', () => {
+            const selectLi = bin.parentElement.parentElement.parentElement;
+            const items = [...document.querySelectorAll(".items")];
+            const indexItems = items.indexOf(selectLi);
+            deleteNote(indexItems);
+            selectLi.remove();
+        });
+        //edit note
+        const edit = li.querySelector(".edit");
+        edit.addEventListener('click', () => {
+            const selectLi = edit.closest(".items");
+            const divTextNote = selectLi.querySelector(".div-text-notes");
+            selectLi.classList.toggle("editing");
+            const items = [...document.querySelectorAll(".items")];
+            const indexEdit = items.indexOf(selectLi);
+            items.forEach(item => {
+                if (item.classList.contains("editing")) {
+                    const inputEdit = document.createElement("input");
+                    inputEdit.type = "text";
+                    inputEdit.classList.add("input-edit");
+                    const textNote = selectLi.querySelector(".text-notes");
+                    inputEdit.value = textNote.textContent;
+                    textNote.classList.add("d-none");
+                    divTextNote.append(inputEdit);
+                    inputEdit.addEventListener('keydown', (e) => {
+                        if (e.key === "Enter") {
+                            textNote.classList.remove("d-none");
+                            textNote.textContent = inputEdit.value.trim();
+                            inputEdit.remove();
+                            selectLi.classList.remove("editing");
+                        }
+                    });
+                }
+            })
+        });
+    });
+}
+
+function deleteNote(index) {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    notes.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function editNote(index) {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    notes.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
 }
 
 document.addEventListener('DOMContentLoaded', NoteApp);
